9034986c88827a8695b9411ceebd181c296e4879
admin <admin@10.137.138.157>
2018-01-02 17:06:18 +0900

    Chapter02

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -1,8 +1,18 @@
 package icehs.science.chapter02;
 
 public class HelloWorld {
+//나의 첫번째 자바 프로그램
 
 	public static void main(String[] args) {
+		//메인 메서드 안에 주석 달기
+		/*여러줄
+		 * 일때는
+		 * 이러한 주석을
+		 * 이용한다
+		 */
+		//이렇게
+		//해도
+		//되거나
 		System.out.println("Hello World!!");
 		System.out.println("안녕하시오?");
 		System.out.println("sysout이라  입력하고 컨트롤 스페이스바를 누르면 간단히 완성됩니다.");
